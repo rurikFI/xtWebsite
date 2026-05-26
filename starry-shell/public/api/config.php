b8558909514744ce467324675c1485f3b5c42795
Rurik <ortendahlrurik@gmail.com>
2026-05-26 14:37:27 +0300

    ADM Label creation v4

--- starry-shell/public/api/config.php
+++ starry-shell/public/api/config.php
@@ -5,5 +5,5 @@ function _env(string $key): string {
 define('STRIPE_SECRET',   _env('STRIPE_SECRET'));
 define('EMAILS_CSV',      __DIR__ . '/../../emails.csv');
 define('POSTI_API_KEY',   _env('POSTI_API_KEY'));
-define('POSTI_CUST_NO',   '1027264');
+define('POSTI_CUST_NO',   '9788790');
 define('ADMIN_SECRET',    _env('ADMIN_SECRET'));
