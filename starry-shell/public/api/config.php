8f5425cef55afb237cab7c3c3378b04ffeba0f3b
Rurik <ortendahlrurik@gmail.com>
2026-05-26 14:59:41 +0300

    ADM Label creation v8

--- starry-shell/public/api/config.php
+++ starry-shell/public/api/config.php
@@ -2,8 +2,15 @@
 function _env(string $key): string {
     return getenv($key) ?: ($_ENV[$key] ?? ($_SERVER[$key] ?? ''));
 }
-define('STRIPE_SECRET',   _env('STRIPE_SECRET'));
-define('EMAILS_CSV',      __DIR__ . '/../../emails.csv');
-define('POSTI_API_KEY',   _env('POSTI_API_KEY'));
-define('POSTI_CUST_NO',   _env('POSTI_CUST_NO'));
-define('ADMIN_SECRET',    _env('ADMIN_SECRET'));
+
+// Load secrets.php if present (gitignored, uploaded manually to server)
+$_secretsFile = __DIR__ . '/secrets.php';
+if (file_exists($_secretsFile)) {
+    require $_secretsFile;
+}
+
+define('STRIPE_SECRET',  defined('_STRIPE_SECRET') && _STRIPE_SECRET ? _STRIPE_SECRET : _env('STRIPE_SECRET'));
+define('POSTI_API_KEY',  defined('_POSTI_API_KEY') ? _POSTI_API_KEY : _env('POSTI_API_KEY'));
+define('POSTI_CUST_NO',  defined('_POSTI_CUST_NO') ? _POSTI_CUST_NO : _env('POSTI_CUST_NO'));
+define('ADMIN_SECRET',   defined('_ADMIN_SECRET')  ? _ADMIN_SECRET  : _env('ADMIN_SECRET'));
+define('EMAILS_CSV',     __DIR__ . '/../../emails.csv');
