32c2834c620339ff68ad37882917b6202a00b006
rurikFI <ortendahlrurik@gmail.com>
2026-05-29 15:15:35 +0200

    Astro settings

--- starry-shell/astro.config.mjs
+++ starry-shell/astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://xtrudertools.com',
+
+  prefetch: true,
+
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
